685a90145cf4d14dcd854c5ae227afc37de175ef
Mixaal <michal.conos@gmail.com>
2020-12-18 11:44:05 +0100

    Enable port for tensorflow

--- jupyterlab-image/run-notebook.sh
+++ jupyterlab-image/run-notebook.sh
@@ -9,6 +9,10 @@ export PYTHONPATH=/usr/local/python/
 
 nohup ipcluster start &
 
+#Enable this one if you need Net2Viz
+#cd /backend
+#nohup python3 ./server.py &
+
 cd /jupyter_root
 nohup jupyter notebook --allow-root --ip 0.0.0.0 --no-browser --port 8888 &
 nohup jupyter lab --ip 0.0.0.0 --no-browser --allow-root --port 8889
--- jupyterlab-image/run.sh
+++ jupyterlab-image/run.sh
@@ -3,4 +3,4 @@
 THEME=${1:-chesterish}
 
 #docker run -it --rm  jupyterlab /bin/bash
-docker run -eSET_THEME="$THEME"  -eMYSQL_PASSWORD="$MYSQL_PASSWORD" --name jupyterlab  -p8889:8889 -p8888:8888 -v "$HOME/git/DL:/notebooks" --rm  jupyterlab
+docker run -eSET_THEME="$THEME"  -eMYSQL_PASSWORD="$MYSQL_PASSWORD" --name jupyterlab  -p6006:6006 -p8889:8889 -p8888:8888 -v "$HOME/git/DL:/notebooks" -v "$HOME/Workspace:/Workspace" --rm  jupyterlab
